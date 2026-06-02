187cef209b30e25f3f51dfe4d20be876f6292959
buberdds <michal@oasisprotocol.org>
2026-06-02 16:40:25 +0200

    Add chain used by Moonpay to shared config

--- apps/preview/src/providers/providers.tsx
+++ apps/preview/src/providers/providers.tsx
@@ -5,7 +5,12 @@ import { WagmiProvider } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RainbowKitProvider, darkTheme, lightTheme } from '@rainbow-me/rainbowkit'
 import { wagmiConfig } from './wagmi-config'
-import { PrivanaProvider, NETWORK_CONFIG, type Network } from '@oasisprotocol/privana-sdk'
+import {
+  PrivanaProvider,
+  NETWORK_CONFIG,
+  SUPPORTED_CHAINS,
+  type Network,
+} from '@oasisprotocol/privana-sdk'
 import { ThemeProvider, useTheme } from './theme-provider'
 import '@rainbow-me/rainbowkit/styles.css'
 
@@ -64,6 +69,7 @@ export function Providers({ children, network = 'testnet' }: ProvidersProps) {
           <RainbowKitWrapper>
             <PrivanaProvider
               networkConfig={{ ...NETWORK_CONFIG[network], apiUrl }}
+              tokens={['0xbd3a41ffd21be1cfcdca7a4e7755842a5b78c9443fb7ea008e6a7314f0caea87']}
               hostedAuth={
                 hostedAuthClientId && hostedAuthRedirectUri
                   ? {
